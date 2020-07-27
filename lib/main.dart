4b152e0158ce8388952b824a8de494c4728ad7fe
Filip Ljubic <f.ljubic7@hotmail.com>
2020-07-27 11:02:23 +0200

    gotovo

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
     MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.red,
+        backgroundColor: Colors.red[300],
         appBar: AppBar(
           title: Text('Dicee'),
-          backgroundColor: Colors.red,
+          centerTitle: true,
+          backgroundColor: Colors.red[300],
         ),
         body: DicePage(),
       ),
@@ -15,9 +18,48 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int random = 1;
+  int random2 = 1;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      alignment: Alignment.center,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Expanded(
+            child: FlatButton(
+              child: Image.asset('images/dice$random.png'),
+              onPressed: () {
+                buttonPressed();
+              },
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              child: Image.asset('images/dice$random2.png'),
+              onPressed: () {
+                buttonPressed();
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  buttonPressed() {
+    setState(() {
+      random = Random().nextInt(6) + 1;
+      random2 = Random().nextInt(6) + 1;
+    });
   }
 }
